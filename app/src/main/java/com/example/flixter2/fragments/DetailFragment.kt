7d2a152b6fcbd670419eaa49fd5f3698257faf49
dev-lester94 <dev.lester94@gmail.com>
2021-05-31 00:19:18 -0700

    orientationsavetimeandvideoid

--- app/src/main/java/com/example/flixter2/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/flixter2/fragments/DetailFragment.kt
@@ -12,14 +12,13 @@ import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.example.flixter2.R
 import com.example.flixter2.databinding.FragmentDetailBinding
-import com.example.flixter2.models.Movie
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.YouTubePlayerTracker
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import okhttp3.Headers
 import org.json.JSONArray
 import org.json.JSONObject
-import kotlin.properties.Delegates
 
 
 class DetailFragment : Fragment() {
@@ -28,10 +27,14 @@ class DetailFragment : Fragment() {
     val YOUTUBE_API_KEY: String =  "AIzaSyCzznGRbunINCP989cE7Q599Ds_aCfw5Sk"
     val VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 
-    lateinit var videoId: String
-    var seconds by Delegates.notNull<Float>()
+
+    lateinit var tracker: YouTubePlayerTracker
 
     lateinit var youTubePlayerView: YouTubePlayerView
+
+    lateinit var youTubePlayerGlobal: YouTubePlayer
+
+    var orientation = false
 
     //lateinit var youTubePlayerFragment: YouTubePlayerFragment
 
@@ -54,26 +57,52 @@ class DetailFragment : Fragment() {
         youTubePlayerView = binding.youtubePlayerView
         lifecycle.addObserver(youTubePlayerView)
 
-        val client: AsyncHttpClient = AsyncHttpClient()
-        client.get(String.format(VIDEOS_URL, args.movie.movieId), object: JsonHttpResponseHandler(){
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                Log.d(TAG,"onSuccess")
-                val jsonObject: JSONObject = json!!.jsonObject
-                val results: JSONArray = jsonObject.getJSONArray("results")
-                if(results.length() == 0){
-                    return
+        var orientation = savedInstanceState?.getBoolean("orientation")
+        if(orientation == null || orientation == false){
+            val client: AsyncHttpClient = AsyncHttpClient()
+            client.get(
+                String.format(VIDEOS_URL, args.movie.movieId),
+                object : JsonHttpResponseHandler() {
+                    override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                        Log.d(TAG, "onSuccess")
+                        val jsonObject: JSONObject = json!!.jsonObject
+                        val results: JSONArray = jsonObject.getJSONArray("results")
+                        if (results.length() == 0) {
+                            return
+                        }
+
+                        var videoId = results.getJSONObject(0).getString("key")
+                        initializeYoutube( videoId, 0F)
+
+
+                    }
+
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        response: String?,
+                        throwable: Throwable?
+                    ) {
+                        Log.d(TAG, "onFailure")
+                    }
+                })
+        }else{
+
+
+            var videoId = savedInstanceState?.getString("videoId")
+            var seconds = savedInstanceState?.getFloat("seconds")
+            if (videoId != null) {
+                Log.i(TAG, videoId)
+            }
+            Log.i(TAG, seconds.toString())
+            if (videoId != null) {
+                if (seconds != null) {
+                    initializeYoutube(videoId, seconds)
                 }
-
-                videoId = results.getJSONObject(0).getString("key")
-                initializeYoutube()
-
-
             }
+        }
 
-            override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?) {
-                Log.d(TAG,"onFailure")
-            }
-        })
+
 
 
 
@@ -81,14 +110,18 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
-    private fun initializeYoutube() {
+    private fun initializeYoutube(videoId: String,seconds: Float) {
         youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
 
                 //youTubePlayerView.videoId
-                Log.i(TAG, videoId)
+                //Log.i(TAG, this@DetailFragment.videoId)
 
-                youTubePlayer.loadVideo(videoId, 0F)
+                youTubePlayer.loadVideo(videoId, seconds)
+                tracker = YouTubePlayerTracker()
+                youTubePlayer.addListener(tracker);
+                youTubePlayerGlobal = youTubePlayer
+
 
             }
         })
@@ -96,11 +129,16 @@ class DetailFragment : Fragment() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("videoId", videoId)
+        outState.putString("videoId", tracker.videoId)
+        outState.putFloat("seconds", tracker.currentSecond)
+        //youTubePlayerGlobal.pause()
+        outState.putBoolean("orientation",true)
 
         //youTubePlayerView.getPlayerUiController().getMenu().
         //outState.putFloat("seconds", seconds)
     }
+
+
 
 
 }
